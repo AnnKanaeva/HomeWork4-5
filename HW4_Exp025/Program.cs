14d5be3f8d1c98cca593dfb1ce6e744db828f483
Ann <anaykanaeva@yandex.ru>
2022-07-19 16:32:54 +0300

    HW4_Exp025

---
+++ HW4_Exp025/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+double GetNumber(string message)
+{
+    double result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (double.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели не число. Введите корректное число.");
+        }
+    }
+    return result;
+}
+double Exp(double numberA, double numberB)
+{
+    double result = 1;
+    for (double i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
+double numberA = GetNumber("Введите число A: ");
+double numberB = GetNumber("Введите число B: ");
+double exponentiation = Exp(numberA, numberB);
+Console.WriteLine($"Число {numberA} в степени {numberB} равно " + exponentiation);
