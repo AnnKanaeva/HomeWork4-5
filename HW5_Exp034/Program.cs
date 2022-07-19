2322f38b9b43c92f4287d82617b0fd33b3cce3aa
Ann <anaykanaeva@yandex.ru>
2022-07-19 22:08:54 +0300

    HW5_Exp034

---
+++ HW5_Exp034/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2 */
+
+Random rnd = new Random();
+int[] array = new int[4];
+int result = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(100, 1000);
+    Console.Write($"{array[i]} ");
+    if (array[i] % 2 == 0)
+    {
+        result = result+1;  
+    }
+}
+Console.WriteLine($"Количество четных чисел массива {result} ");
+
