0a160d86b594f1e118f0c3089d728925781a408b
Ann <anaykanaeva@yandex.ru>
2022-07-22 15:19:21 +0300

    HW6_Exp041

---
+++ HW6_Exp041/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели символ либо не целое число. Пожалуйста, введите корректное число.");
+        }
+    }
+    return result;
+}
+int m = GetNumber($"Введите число М(количество чисел): ");
+int[] massiveNumbers = new int[m];
+
+void InputNumbers(int m)
+{
+    for (int i = 0; i < m; i++)
+    {
+        massiveNumbers[i] = GetNumber($"Введите {i + 1} число: ");
+    }
+}
+
+
+int Comparison(int[] massiveNumbers)
+{
+    int count = 0;
+    for (int i = 0; i < massiveNumbers.Length; i++)
+    {
+        if (massiveNumbers[i] > 0) count += 1;
+    }
+    return count;
+}
+
+InputNumbers(m);
+
+Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
+
+
